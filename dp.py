32f8e616bd6783d7b2f43ee9e5464758d8a2b1e7
pangyd <842590859@qq.com>
2024-08-27 01:19:59 +0800

    update

--- dp.py
+++ dp.py
@@ -239,3 +239,18 @@ def climbStairs(n: int) -> int:
     for i in range(2, n + 1):
         f.append(f[i - 1] + f[i - 2])
     return f[-1]
+
+
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    """1143.最长公共子序列"""
+    n1, n2 = len(text1), len(text2)
+    dp = [[0] * (n1+1) for _ in range(n2+1)]
+
+    dp[0][0] = 0
+    for i in range(1, n2+1):
+        for j in range(1, n1+1):
+            if text1[j-1] == text2[i-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp[n2][n1]
