0dcdb160f74b859838f541510766f85e685db5b0
pangyd <842590859@qq.com>
2024-09-01 16:21:08 +0800

    update

--- bitwise.py
+++ bitwise.py
@@ -5,3 +5,16 @@
 @time: 2024/9/1 15:38
 @desc: 
 """
+
+
+def divide(self, dividend: int, divisor: int) -> int:
+    """29.求除数"""
+    if dividend == -2147483648 and divisor == -1:
+        return 2147483647
+    a, b, res = abs(dividend), abs(divisor), 0
+    # 2 ** i * b <= a 《==》 a / b = 2 ** i + (a - 2 ** i * b) / b
+    for i in range(31, -1, -1):
+        if (b << i) <= a:
+            res += 1 << i
+            a -= b << i
+    return res if (dividend > 0) == (divisor > 0) else -res
